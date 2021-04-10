bb9d26014140a75473e541f0638f7818629b7ece
Kübra PETEK <kubrapetekk@gmail.com>
2021-04-10 18:58:11 +0300

    İş Katmanı İmplementasyonu

---
+++ MemberRegistration.Business/Concreate/MemberManager.cs
@@ -0,0 +1,24 @@
+﻿using MemberRegistration.Business.Abstract;
+using MemberRegistration.DataAccess.Abstract;
+using MemberRegistration.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.Business.Concreate
+{
+    public class MemberManager : IMemberService
+    {
+        IMemberDal _memberDal;
+        public MemberManager(IMemberDal memberDal)
+        {
+            _memberDal = memberDal;
+        }
+        public void Add(Member member)
+        {
+            _memberDal.Add(member);
+        }
+    }
+}
