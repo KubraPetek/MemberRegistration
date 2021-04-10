d9cc7b69bb574cd3a7828b292888e79f114cefcc
Kübra PETEK <kubrapetekk@gmail.com>
2021-04-10 19:47:30 +0300

    KpsService kullanımı ve Microservis mimarisi implementasyonu

--- MemberRegistration.Business/Concreate/MemberManager.cs
+++ MemberRegistration.Business/Concreate/MemberManager.cs
@@ -1,4 +1,6 @@
 ﻿using MemberRegistration.Business.Abstract;
+using MemberRegistration.Business.KpsServiceReference;
+using MemberRegistration.Business.ServiceAdapters;
 using MemberRegistration.DataAccess.Abstract;
 using MemberRegistration.Entities.Concreate;
 using System;
@@ -12,13 +14,22 @@ namespace MemberRegistration.Business.Concreate
     public class MemberManager : IMemberService
     {
         IMemberDal _memberDal;
-        public MemberManager(IMemberDal memberDal)
+        IKpsService _kpsService;
+        public MemberManager(IMemberDal memberDal, IKpsService kpsService)
         {
             _memberDal = memberDal;
+            _kpsService = kpsService;
         }
         public void Add(Member member)
         {
+            //KPSPublicSoapClient client = new KPSPublicSoapClient();  ---Miktoservis miamarisine ters , servisin değişme ihtimali var 
+            //client.TCKimlikNoDogrula();
+            if (!_kpsService.ValidateUser(member))
+            {
+                throw new Exception("Kullanıcı doğrulaması geçerli değil!");
+            }
             _memberDal.Add(member);
         }
+
     }
 }
---
+++ MemberRegistration.Business/ServiceAdapters/IKpsService.cs
@@ -0,0 +1,15 @@
+﻿using MemberRegistration.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.Business.ServiceAdapters
+{
+    public interface IKpsService //Kps'in anlamı kimlik paylaşım sistemi
+    {
+        bool ValidateUser(Member member);
+
+    }
+}
